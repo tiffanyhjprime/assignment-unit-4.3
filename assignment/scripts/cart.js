ae41aab9f9049154564b81ae8327f5925505ecbe
Tiffany House-Jimenez <tiffanyhousejimenez@gmail.com>
2023-11-19 18:17:16 -0600

    answered required questions. Got tests to pass, but I'm not sure how or if they are fully correct. Need more practice.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,57 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//Create a global variable named basket and set it to an empty array.
+const basket = [];
+
+//Create a function called addItem. It should:
+
+//take an input parameter for a string item
+//add the new item to the global array basket.
+//return true indicating the item was added
+
+function addItem(item) {
+    console.log('in addItem');
+    let addItem = basket.push(item);
+    if (basket.length >0 );
+        return true;
+}
+
+console.log('test addItem - output should be true:', addItem('fruit'));
+console.log(basket);
+
+//add more items to the basket to test if they're being added to array
+addItem('vegetables');
+addItem('chicken');
+console.log('test basket array after adding chicken and vegetables',basket);
+
+//Create a function called listItems. It should:
+//loop over the items in the basket array
+//console.log each individual item on a new line
+
+function listItems(basket){
+    console.log('in listItem');
+    for(i of basket);
+    console.log(basket);
+} 
+console.log('test listItems:', listItems);
+
+
+
+
+//Create a function called empty. It should:
+//reset the basket to an empty array
+
+function empty() {
+    console.log('in empty');
+    for(let i = 0; i < basket.length+4; i++) {
+        console.log('items are removed', basket.pop());
+    };
+ 
+}
+
+
+
 
 
 
